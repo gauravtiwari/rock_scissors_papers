3c2b225af1961c976d5301f6189a848671ca8cfd
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-09-25 13:53:26 +0100

    ENV vars for Heroku redis update

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 require 'connection_pool'
-Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis::Namespace.new("rsp_#{Rails.env.downcase}", :redis => Redis.new(:url => (ENV["REDIS_URL"]))) }
+Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) { Redis::Namespace.new("rsp_#{Rails.env.downcase}", :redis => Redis.new(:url => (ENV["HEROKU_REDIS_NAVY_URL"]))) }
