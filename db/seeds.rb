4c55713a97b95b0f5a1081c200628b58c9d7efee
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-09-25 14:21:17 +0100

    remove duplicate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@
 # => Seed some players
 Player.create!(name: 'Computer', nickname: 'computer', email: 'computer@example.com', password: 'password')
 Player.create!(name: 'John Doe', nickname: 'john', email: 'john@example.com', password: 'password')
-Player.create!(name: 'Doe John', nickname: 'doe', email: 'john@example.com', password: 'password')
+Player.create!(name: 'Doe John', nickname: 'doe', email: 'doe@example.com', password: 'password')
